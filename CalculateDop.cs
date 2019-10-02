a9b9817008171d869d4a45915db74f7a61068eab
Niyazik <fexfulfexful@gmail.com>
2019-10-02 23:41:54 +0300

    gitflow-feature-stash2: calcfeature1

--- CalculateDop.cs
+++ CalculateDop.cs
@@ -3,6 +3,9 @@ using System;
 namespace Lab_1
 {
     public class CalculateDop{
-        
+        public double Pow(ref double n1, ref double n2){
+            n1 = Math.Pow(n1,n2);
+            return n1;
+        }
     }
 }
