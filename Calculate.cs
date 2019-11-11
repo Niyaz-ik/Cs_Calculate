c4c796617aaa733df8e6d61233ba5a4a2fd51ef7
Konstantin <kkletsun@gmail.com>
2019-11-12 00:17:45 +0300

    Feature added

--- Calculate.cs
+++ Calculate.cs
@@ -9,6 +9,7 @@ namespace Lab_1
         public double Minus(ref double n1, ref double n2){
             n1-= n2;
             return n1;
+            // kekss
         }
 
         public double Multiply(ref double n1, ref double n2){
@@ -23,8 +24,8 @@ namespace Lab_1
         public double Residue(ref double n1, ref double n2){
             n1%= n2;
             return n1;
-            // test comment 123
-            // kekss
+            // test comment 123 456 789
+            // add some real code (no)
         }
     }
 }
